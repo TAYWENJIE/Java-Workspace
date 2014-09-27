429f21feced0ffaeb3a150e2ac5629e87bf3bb2b
TAYWENJIE <taywenjie@hotmail.comn>
2014-09-27 21:38:21 +0800

    Revert "Revert "Beginner Tutorial From thenewbonston""
    This reverts commit df67d2dd64d72061869596f9dbaf4d5d6340a3fb.

---
+++ Java Beginner Tut 6-Getting User Input/src/apples.java
@@ -0,0 +1,15 @@
+/*
+ import Scanner Class to get user input
+ */
+import java.util.Scanner;
+
+public class apples {
+
+	public static void main(String[] args) {
+		Scanner message=new Scanner(System.in); //object name as message 
+		System.out.println(message.nextLine()); // print out message after getting I/P from user
+		
+
+	}
+
+}

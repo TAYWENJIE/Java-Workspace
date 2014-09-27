d478b1309c9d8c4adc56794290822b5e4e7e03d0
TAYWENJIE <taywenjie@hotmail.comn>
2014-09-27 21:37:08 +0800

    Beginner Tutorial From thenewbonston
    Youtube

---
+++ Java Beginner Tut 4-HelloWorld/src/apples.java
@@ -0,0 +1,14 @@
+/*
+ System.out.println - Anything Print statement after move to next line
+ System.out.print -Anything Print statement after stay in same line
+ */
+public class apples {
+//java always start from main method 
+	public static void main(String[] args) {
+		System.out.println("Hello World!"); // Print before new line 
+		System.out.print("Hi");				// con't from the same line 
+		System.out.print("\t i'm WEN JIE");
+
+	}
+
+}
